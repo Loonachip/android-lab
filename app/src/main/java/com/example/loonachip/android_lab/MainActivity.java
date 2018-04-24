2a7809955cfc836dd1673b6ec127c49d49f2afbd
Loonachip <sjastrzebski96@gmail.com>
2018-04-24 10:48:18 +0200

    comment added

--- app/src/main/java/com/example/loonachip/android_lab/MainActivity.java
+++ app/src/main/java/com/example/loonachip/android_lab/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //leleeeleleleel
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
