7af0131cb1e27505458bafcc21275a1ada70f44b
Loonachip <sjastrzebski96@gmail.com>
2018-05-14 20:10:10 +0200

    lab5 second commit

--- app/src/main/java/com/example/loonachip/android_lab/MainActivity.java
+++ app/src/main/java/com/example/loonachip/android_lab/MainActivity.java
@@ -1,15 +1,69 @@
 package com.example.loonachip.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> target;
+    private SimpleCursorAdapter adapter;
+    private MySQLite db;
 
-    //leleeeleleleel
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] values = new String[]{"q", "w", "e", "r", "t", "y"};
+
+        this.db = new MySQLite(this);
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                db.lista(),
+                new String[] {"_id", "gatunek"},
+                new int[] {android.R.id.text1, android.R.id.text2},
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+        );
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent data){
+        if(reqCode==1 && resCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public void nowyWpis(MenuItem mi){
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 }
